bd03ad96dd7746a09d1d18728a519cbb52edad51
Samaji-2003 <samajidulmini@gmail.com>
2025-10-21 20:35:20 +0530

    control flow 2
    switch, string methods

--- 02 - Control Flow/index.js
+++ 02 - Control Flow/index.js
@@ -1,3 +1,4 @@
+
 //if statement
 let age;
 document.getElementById("checkButton").onclick = function(){
@@ -37,3 +38,113 @@ document.getElementById("btn").onclick = function(){
         document.getElementById("output").innerText = "Checkbox is not checked.";
     }
 }
+
+// ternary operator
+
+let q = 10;
+let msg = q>5 ? "q is greater than 5" : "q is 5 or less";
+console.log(msg);
+
+let school = isStudent ? "You go to school." : "You don't go to school.";
+console.log(school);
+
+//switch statement
+let day = 4;
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+let testScore = 85;
+let grade;
+
+switch(true){
+    case testScore >= 75:
+        grade = "A";
+        break;
+    case testScore >= 60:
+        grade = "B";
+        break;
+}  
+
+//string methods
+
+let userName = "samaji";
+
+userName.charAt(0); // "s"
+console.log(userName.charAt(0));
+
+userName.indexOf("m"); // 2
+console.log(userName.indexOf("m"));
+
+userName.length; // 6
+console.log(userName.length);
+
+userName.trim(); // "samaji"
+console.log(userName.trim());
+
+userName.toUpperCase(); // "SAMAJI  "
+console.log(userName.toUpperCase());
+
+userName.toLowerCase(); // "samaji  "
+console.log(userName.toLowerCase());
+
+userName = userName.repeat(5); // "samajisamajisamajisamajisamaji"
+console.log(userName.repeat(5));
+
+let result = userName.startsWith("s"); // true
+console.log(result);
+
+result = userName.endsWith("i"); // true
+console.log(result);
+
+result = userName.includes("m"); // true
+console.log(result);
+
+let newUserName = userName.replace("s", "S"); // "Samaji  "
+console.log(newUserName);
+
+userName = userName.padStart(10, "*"); // "***samaji  "
+
+
+//string slicing
+
+let fruit = "Watermelon";
+let word1 =fruit.slice(0,5); // "Water"
+console.log(word1);
+
+let word2 = fruit.slice(5, 10); // "melon"
+console.log(word2);
+
+let word3 = fruit.slice(-3); // "lon"
+console.log(word3);
+
+let fruit2 = "dragon fruit"; 
+let word4 = fruit2.slice(0,fruit2.indexOf(" ")); // "dragon"
+console.log(word4);
+
+let email = "samaji@example.com";
+let emailUser = email.slice(0, email.indexOf("@")); // "samaji"
+console.log(emailUser);
