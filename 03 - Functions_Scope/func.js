cc17f9a5a16bc7ef839bf5c8222c161f6133f017
Samaji-2003 <samajidulmini@gmail.com>
2025-10-26 19:39:47 +0530

    tem converter
    funct cnt.

--- 03 - Functions_Scope/func.js
+++ 03 - Functions_Scope/func.js
@@ -1,3 +1,5 @@
+const { Fragment } = require("react/jsx-runtime");
+
 function hbd(name){
     console.log("Happy Birthday to You!");
     console.log("Happy Birthday to You!");
@@ -16,4 +18,28 @@ function isValidEmail(email){
     return false;
 }
 
-console.log(isValidEmail(ex@gmail.com))
+console.log(isValidEmail("example@gmail.com"));
+
+//temparature converter
+
+
+
+function convertTemperature(){
+    
+    const temp = Number(document.getElementById("Temperature").value);
+    const toC = document.getElementById("toC");
+    const toF = document.getElementById("toF");
+
+
+    if(toC.checked){
+        const celsius = (temp - 32) * 5/9;
+        document.getElementById("result").textContent = celsius;
+    }
+    else if(toF.checked){
+        const fahrenheit = (temp * 9/5) + 32;
+        document.getElementById("result").textContent = fahrenheit;
+    }
+    else{
+        alert("Please select a conversion type.");
+    }
+}
