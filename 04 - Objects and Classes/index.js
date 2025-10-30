161f9cf149b1929274dc33da5c2ca2c453a2c003
Samaji-2003 <samajidulmini@gmail.com>
2025-10-30 09:37:57 +0530

    destructors , array of objects, nested objects

--- 04 - Objects and Classes/index.js
+++ 04 - Objects and Classes/index.js
@@ -267,3 +267,138 @@ class Person{
 
 const person1 = new Person("samaji", 22);
 console.log("Person 1:", person1);
+
+//destructuring
+
+//swap the values
+
+let a = 5;
+let b = 10;
+
+[a, b] = [b, a];
+console.log("a:", a, "b:", b);
+
+const color = ["red", "green", "blue", "yellow", "purple"];
+
+[color[0], color[2] ] = [color[2], color[0]];
+console.log("Colors:", color);
+
+const [col1, col2, col3, ...rest] = color;
+console.log("col1:", col1, "col2:", col2, "col3:", col3);
+console.log("rest:", rest);
+
+const personObj1 = {
+    f_name: "samaji",
+    l_name: "dulmoni",
+    age1: 22,
+    city: "New York",
+    country: "USA"
+};
+
+ const personObj2 = {
+    fname: "rahul",
+    lname: "sharma",
+    age2: 25,
+    city: "Los Angeles"
+ };
+
+//extracting values
+const {f_name, l_name, age1, ...address} = personObj1;
+console.log(f_name);
+console.log(l_name);
+console.log(age1);
+console.log(address);
+
+const {fname, lname, age2, city, country = "Unknown"} = personObj2;
+console.log(fname);
+console.log(lname);
+console.log(age2);
+console.log(city);
+console.log(country);
+
+function displayPerson({fname, lname, age2, city, country}){
+    console.log(fname);
+    console.log(lname);
+    console.log(age2);
+    console.log(city);
+    console.log(country);
+}
+
+displayPerson(personObj2);
+
+//nested object
+
+const employee = {
+    id: 101,
+    name: "samaji dulmini",
+    position: "developer",
+    hobbies: ["reading", "traveling", "coding"],
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        zip: "10001"
+    }
+};
+
+console.log(employee.hobbies);
+console.log(employee.address.city);
+
+class Employee{
+    constructor(name, position, ...address){
+        this.name = name;
+        this.position = position;
+        this.address = new Address(...address);// call the Address class
+    }
+}
+
+class Address{
+
+    constructor(street, city, country){
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+const emp1 = new Employee("rahul sharma", "designer", "456 Market St", "Los Angeles", "USA");
+
+console.log(emp1.address.country);
+
+//array of objects
+
+const fruits = [{name: "Apple", color: "Red", calories: 95},
+    {name: "Banana", color: "Yellow", calories: 105},
+    {name: "Orange", color: "Orange", calories: 62},
+    {name: "Grapes", color: "Purple", calories: 69},
+]
+
+
+console.log(fruits[0].name);
+console.log(fruits[2].calories);
+
+fruits.push({name: "Mango", color: "Yellow", calories: 150});
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.splice(1,2); //remove 2 elements from index 1
+console.log(fruits);
+
+fruits.forEach(fruit => console.log(fruit.name));
+
+
+//map
+
+const fruitName = fruits.map(fruit => fruit.name);
+console.log("Fruit Names:", fruitName);
+
+
+const purpleFruits = fruits.filter(fruit => fruit.color === "Purple").map(fruit => fruit.name);
+
+//reduce
+
+const maxCalories = fruits.reduce((accumulator, fruit) => 
+                                     accumulator < fruit.calories ? 
+                                   fruit.calories : accumulator);
+console.log("Max Calories:", maxCalories);
